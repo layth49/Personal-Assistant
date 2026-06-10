dffc0f7a97abcf936a1bb8ddf3eded61215f4f99
49 <laythandamin@gmail.com>
2026-06-10 05:20:34 -0500

    Set Python DLL to absolute user path
    Replace relative Runtime.PythonDLL with an absolute path to the user's Python 3.12 installation (C:\Users\layth\AppData\Local\Programs\Python\Python312\python312.dll). This hardcodes a machine-specific location which may reduce portability; consider using configuration or environment detection.

--- Program.cs
+++ Program.cs
@@ -104,7 +104,7 @@ namespace Personal_Assistant
             // 49 (ASCII art)
             Console.WriteLine("                                    \r\n     ,AM  .d*\"*bg.\r\n    AVMM 6MP    Mb\r\n  ,W' MM YMb    MM\r\n,W'   MM  `MbmmdM9\r\nAmmmmmMMmm     .M'\r\n      MM     .d9  \r\n      MM   m\"'    \n\n");
 
-            Runtime.PythonDLL = @"..\..\..\..\AppData\Local\Programs\Python\Python312\python312.dll";
+            Runtime.PythonDLL = @"C:\Users\layth\AppData\Local\Programs\Python\Python312\python312.dll"; ;
             PythonEngine.Initialize();
             PythonEngine.BeginAllowThreads();
 
